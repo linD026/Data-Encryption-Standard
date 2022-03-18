8422851fe369fda8e698feddd29d86014fc6872c
linD026 <shiyn.lin@gmail.com>
2022-03-18 10:53:22 +0800

    Fix the digit print format
    It need to print out of 8 bytes(64 bits) each time loop, but the
    while loop is doesn't increase and the size is decrease 64 bytes
    each time.

--- des.c
+++ des.c
@@ -44,9 +44,12 @@ static struct des_struct des_ctx = {
 
 static inline void print_int(uint8_t *p, size_t size)
 {
+    uint64_t *__p = (uint64_t *)p;
+    printf("digit: \"");
     do {
-        printf("%llu ", *(uint64_t *)p);
-    } while (size -= 64, size > 0);
+        printf("%llu ", *__p);
+    } while (__p += 1, size -= 8, size > 0);
+    printf("\"\n");
 }
 
 void des_encrypt(uint32_t *key, uint8_t *dst, uint8_t *src)
@@ -173,9 +176,7 @@ static inline void do_des_encrypt(void)
         des_encrypt((uint32_t *)des_ctx.key, (uint8_t *)dst_p, (uint8_t *)src_p);
     } while (dst_p += 8, src_p += 8, size -= 8, size > 0);
     printf("Encrypt text:\n");
-    printf("digit: \"");
     print_int(des_ctx.output, des_ctx.ceil_size);
-    printf("\"\n");
     printf("text: \"%s\"\n", des_ctx.output);
 }
 
@@ -188,9 +189,7 @@ static inline void do_des_decrypt(void)
         des_decrypt((uint32_t *)des_ctx.key, (uint8_t *)dst_p, (uint8_t *)src_p);
     } while (dst_p += 8, src_p += 8, size -= 8, size > 0);
     printf("Decrypt text:\n");
-    printf("digit: \"");
     print_int(des_ctx.output, des_ctx.ceil_size);
-    printf("\"\n");
     printf("text: \"%s\"\n", des_ctx.output);
 }
 
@@ -223,11 +222,10 @@ static inline void get_text(int opt)
         pr_err("malloc failed (output)\n");
     memset(des_ctx.output, 0, des_ctx.ceil_size);
 
-    printf("original text (ceil size %zu, size %zu):\n", des_ctx.ceil_size,
+    printf("original text (ceil size %zu, size %zu):\n\n", des_ctx.ceil_size,
            des_ctx.size);
-    printf("\ndigit: \"");
     print_int(des_ctx.input, des_ctx.ceil_size);
-    printf("\"\n\n");
+    printf("\n");
 }
 
 static void test_en_de(void)
@@ -243,11 +241,12 @@ static void test_en_de(void)
     orig_src_p = des_ctx.input;
     des_ctx.input = des_ctx.output;
     des_ctx.output = encypted;
+    printf("\n");
     
     do_des_decrypt();
 
     if (strncmp((char *)orig_src_p, (char *)des_ctx.output, 64) == 0)
-        printf("Passed\n");
+        printf("\nPassed\n");
 
     des_ctx.output = orig_src_p;
     free(encypted);
